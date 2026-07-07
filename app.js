1f71561780668cf25614343e870593efee5f3e10
meditationski <meditationski@gmail.com>
2026-07-07 11:03:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -121,8 +121,8 @@ const state = {
     },
 
     bookingPeriod: {
-        startDate: new Date('2026-07-07'),
-        endDate: new Date('2026-07-10')
+        startDate: new Date('2026-12-24'),
+        endDate: new Date('2027-04-10')
     }
 };
 
